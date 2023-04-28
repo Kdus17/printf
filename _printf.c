e2b2a4e12de5b9d1139cf4fb5328b4af17990e6f
kidus <kidus2460@gmail.com>
2023-04-28 13:00:00 -0700

    kl

---
+++ _printf.c
@@ -0,0 +1,42 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+int _printf(const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    int count = 0;
+    while (*format != '\0') {
+        if (*format == '%') {
+            format++;
+            switch (*format) {
+                case 'c': {
+                    char c = (char)va_arg(args, int);
+                    putchar(c);
+                    count++;
+                    break;
+                }
+                case 's': {
+                    char *s = va_arg(args, char *);
+                    while (*s != '\0') {
+                        putchar(*s);
+                        s++;
+                        count++;
+                    }
+                    break;
+                }
+                case '%': {
+                    putchar('%');
+                    count++;
+                    break;
+                }
+            }
+        } else {
+            putchar(*format);
+            count++;
+        }
+        format++;
+    }
+    va_end(args);
+    return count;
+}
